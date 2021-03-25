ec952288b5190fe96b0fc5fdf32ec58bc4408a8c
Viktor Belous <56500587+vyntyk@users.noreply.github.com>
2021-03-25 21:47:33 +0300

    Bus tour

--- Problems/Initializing an array of characters/src/Proba.java
+++ Problems/Initializing an array of characters/src/Proba.java
@@ -2,15 +2,13 @@ import java.util.Arrays;
 
 public class Proba {
     public static void main(String[] args) {
-        int[] numbers1 = { 1, 2, 3, 4 };
-        int[] numbers2 = { 1, 2, 3, 4 };
-        int[] numbers3 = { 4, 3, 2, 1 };
-        int[] numbers4 = { 1, 2, 3 };
-
-        System.out.println(Arrays.equals(numbers3, numbers1));
-        System.out.println(Arrays.equals(numbers1, numbers1));
-        System.out.println(Arrays.equals(numbers1, numbers2));
-        System.out.println(Arrays.equals(numbers2, numbers3));
-        System.out.println(Arrays.equals(numbers1, numbers4));
+        for (int i = 0; i < 5; i++) {
+            for (char letter = 'a'; letter <= 'f'; letter++) {
+                if (letter < 'f') {
+                    continue;
+                }
+                System.out.println(letter);
+            }
+        }
     }
 }
