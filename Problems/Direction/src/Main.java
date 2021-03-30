17659883c27ccb4263dc9cd4981dded4d9cd197e
Viktor Belous <56500587+vyntyk@users.noreply.github.com>
2021-03-30 21:14:49 +0300

    Potter

---
+++ Problems/Direction/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        switch (num) {
+            case 0:
+                System.out.println("do not move");
+                break;
+            case 1:
+                System.out.println("move up");
+                break;
+            case 2:
+                System.out.println("move down");
+                break;
+            case 3:
+                System.out.println("move left");
+                break;
+            case 4:
+                System.out.println("move right");
+                break;
+            default:
+                System.out.println("error!");
+                break;
+        }
+    }
+}
